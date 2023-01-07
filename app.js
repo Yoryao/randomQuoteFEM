04d9f83ecdfd1cc1d5bd458afd2a1913cfe316ae
Jorge Rivera <jrivera@VESSELARG.local>
2023-01-07 00:07:41 -0300

    first commit

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 console.log("app working")
 
+let initialQuote = '"It is easy to sit up and take notice, what´s difficult is getting up and taking action"'
 
-            let number = document.getElementById("adviceNumber")
-            let quote = document.getElementById("quote");
+let number = document.getElementById("adviceNumber")
+let quote = document.getElementById("quote");
 let dice = document.getElementsByTagName("img");
 
 let randomNumber = 0;
 
 
 function askAdvice() {
-    console.log("Boton Presionado.")
-
     randomNumber = Math.random()*240;
     randomNumber = Math.floor(randomNumber);
 
-    console.log(randomNumber);
 
     fetch("https://api.adviceslip.com/advice/" + randomNumber)
         .then((res) => res.json())
@@ -24,17 +22,7 @@ function askAdvice() {
             let adviceQuote = objeto.advice;
 
             number.innerText = adviceId;
-            quote.innerText = adviceQuote;
-
-
-
-        })
-    
-
-
-
+            quote.innerText = '"'+adviceQuote + '"';
+        }
+    )
 }
-
-
-
-
